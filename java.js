7808846237d061c557ddb672c5d6407ebfbe215d
Bond <augustobond950@gmail.com>
2025-05-26 17:05:52 -0300

    segunda actualizacion

--- java.js
+++ java.js
@@ -1,5 +1,7 @@
 const container = document.querySelector(".container")
+console.log(container);
 for(let i=0; i < 256; i++){
     const cuadrado=document.createElement("div")
     container.appendChild(cuadrado)
+    
     }
