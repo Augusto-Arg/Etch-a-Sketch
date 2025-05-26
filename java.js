fce3eb904a1bee6d24d591436926ccaf3ee52539
Bond <augustobond950@gmail.com>
2025-05-26 17:28:35 -0300

    actualizacion

--- java.js
+++ java.js
@@ -2,6 +2,12 @@ const container = document.querySelector(".container")
 console.log(container);
 for(let i=0; i < 256; i++){
     const cuadrado=document.createElement("div")
-    container.appendChild(cuadrado)
+     container.appendChild(cuadrado)
+cuadrado.addEventListener("mouseenter", () =>{
+    cuadrado.style.backgroundColor ="lightblue";
+    div.addEventListener("mouseleave", () => {
+  div.style.backgroundColor = ""; // Esto quita el color de fondo
+    })
     
-    }
+})
+    };
